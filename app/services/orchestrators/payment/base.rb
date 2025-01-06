514c5ca68a23d95a3207c972860481688dfb0cf5
erikosampaio <erikoa.93@gmail.com>
2025-01-06 14:10:20 -0300

    Create Orchestrators for payment services.

---
+++ app/services/orchestrators/payment/base.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Orchestrators
+  module Payment
+    class Base
+      def initialize(params)
+        @params = params
+      end
+
+      def call
+        integration = select_integration(@params[:provider])
+
+        return { success: false, errors: 'Invalid provider' } unless integration
+
+        integration.new(@params).call
+      end
+
+      private
+      def select_integration(provider)
+        raise(NotImplementedError)
+      end
+    end
+  end
+end
---
+++ app/services/orchestrators/payment/emission_orchestrator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Orchestrators
+  module Payment
+    class EmissionOrchestrator < Base
+
+      private
+      def select_integration(provider)
+        case provider
+        when :pag_seguro
+          Integrations::PagSeguro::Payment::Emission
+        when :mercado_pago
+          Integrations::MercadoPago::Payment::Emission
+        end
+      end
+    end
+  end
+end
