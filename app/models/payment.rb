2f6a0153a6ca8cb0f0c097349b9692503448a947
erikosampaio <erikoa.93@gmail.com>
2025-01-06 15:06:13 -0300

    Add validates presence to status.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,6 +2,6 @@ class Payment < ApplicationRecord
 
   enum status: [:pending, :paid]
 
-  validates :name, :number, :due_date, :cvc, :amount, presence: true
+  validates :name, :number, :due_date, :cvc, :amount, :status, presence: true
   validates :number, length: { is: 16 }
 end
