6acb065421bb6b2727ebde8823589423fcf7fe31
erikosampaio <erikoa.93@gmail.com>
2025-01-07 10:25:58 -0300

    Adjust serializer from payment

--- app/serializers/payment_serializer.rb
+++ app/serializers/payment_serializer.rb
@@ -1,8 +1,12 @@
 class PaymentSerializer
   include JSONAPI::Serializer
-  attributes :name, :amount, :status
+  attributes :name, :number, :amount, :status
 
   attribute :number do |object|
     object.number[-4..-1]
+  end
+
+  attribute :amount do |object|
+    object.amount.to_f
   end
 end
