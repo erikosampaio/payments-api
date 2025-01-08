89962d336a51c39c660d061db4286baff851b53a
erikosampaio <erikoa.93@gmail.com>
2025-01-08 10:14:10 -0300

    Adjust method logout to login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,21 +16,20 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    if request.headers['Authorization'].present?
-      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
-      current_user = User.find(jwt_payload['sub'])
+    authorization_header = request.headers['Authorization']
+
+    if authorization_header.nil?
+      render json: { message: "Couldn't find an active session." }, status: :unauthorized
+      return
     end
 
-    if current_user
-      render json: {
-        status: 200,
-        message: 'Logged out successfully.'
-      }, status: :ok
-    else
-      render json: {
-        status: 401,
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
+    begin
+      jwt_payload = JWT.decode(authorization_header.split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
+      current_user = User.find(jwt_payload['sub'])
+      sign_out(current_user)
+      render json: { message: 'Logged out successfully.' }, status: :ok
+    rescue JWT::DecodeError, JWT::VerificationError
+      render json: { message: "Couldn't find an active session." }, status: :unauthorized
     end
   end
 end
