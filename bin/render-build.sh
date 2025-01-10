70129876a47c65dc9f1fef9f5437d4161c495917
erikosampaio <erikoa.93@gmail.com>
2025-01-09 22:06:24 -0300

    Fix to configuration database in deploy `render.com`

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,14 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+# Verifica se o banco de dados está pronto
+echo "Waiting for PostgreSQL to be ready..."
+while ! pg_isready -h $POSTGRES_HOST -p 5432 -q -U $POSTGRES_USER; do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 2
+done
+
+# Prepara o banco de dados
+echo "Setting up database..."
 bundle exec rails db:migrate
